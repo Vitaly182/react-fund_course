e0b1c552204895cc164faf8a69c0baa147b145be
Vitaly <w.182@mail.ru>
2022-01-19 01:03:29 +0300

    Первый функциональный компонент

--- src/App.js
+++ src/App.js
@@ -1,30 +1,17 @@
 import React, { useState } from "react";
+import Counter from "./components/Counter";
 
 function App() {
   const [likes, setLikes] = useState(0);
   const [value, setValue] = useState('Текст в input');
-  console.log(likes);
-  console.log(setLikes);
 
-  function increment() {
-    setLikes(likes + 1)
-  }
-
-  function decrement() {
-    setLikes(likes - 1)
-  }
 
   return (
     <div className="App">
-      <h1>{likes}</h1>
-      <h1>{value}</h1>
-      <input 
-        type="text" 
-        value={value} 
-        onChange={event => setValue(event.target.value)}
-      />
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
+      <Counter />
+      <Counter />
+      <Counter />
+
     </div>
   );
 }
