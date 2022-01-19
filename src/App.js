ef9a558fe63959e96ff645842f95bc0e83feaab8
Vitaly <w.182@mail.ru>
2022-01-19 03:05:00 +0300

    Props. Аргументы компонента

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import ClassCounter from "./components/ClassCounter";
 import Counter from "./components/Counter";
+import PostItem from "./components/PostItem";
 import './styles/App.css';
 
 function App() {
@@ -9,17 +10,10 @@ function App() {
 
   return (
     <div className="App">
-      <div className="post">
-        <div className="post__content">
-          <strong>1. Javascript</strong>
-          <div>
-            Javascript - язык программирования
-          </div>
-        </div>
-        <div className="post__btns">
-          <button>Удалить</button>
-        </div>
-      </div>
+      <PostItem post={{id: 1, title: 'Javascript', body: 'Description'}}/>
+      <PostItem post={{id: 2, title: 'Javascript', body: 'Description'}}/>
+      <PostItem post={{id: 3, title: 'Javascript', body: 'Description'}}/>
+
     </div>
   );
 }
