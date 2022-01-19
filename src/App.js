603539e4552a0a0a914cda3ace605ac44aaa8209
Vitaly <w.182@mail.ru>
2022-01-19 23:52:55 +0300

    Обмен данными между компонентами. От родителя к ребенку. От ребенка к родителю.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import PostList from "./components/PostList";
 import './styles/App.css';
 import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MyInput";
+import PostForm from "./components/PostForm";
 
 function App() {
   const [posts, setPosts] = useState([
@@ -14,43 +15,21 @@ function App() {
     {id: 3, title: 'Javascript 3', body: 'Description 3'},
   ])
 
-  const [post, setPost] = useState({title: '', body: ''})
-
-
-  // const bodyInputRef = useRef();
-
-
-  const addNewPost = (e) => {
-    e.preventDefault()
-    setPosts([...posts, {...post, id: Date.now()}])
-    setPost({title: '', body: ''})
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost])
   }
+
+  //Получаем post из дочернего компонента
+  const removePost = (post) => {
+    setPosts(posts.filter(p => p.id !== post.id))
+  }
+
+
 
   return (
     <div className="App">
-      <form>
-        {/* Управляемый компонент */}
-        <MyInput 
-          value={post.title}
-          onChange={e => setPost({...post,  title: e.target.value})}
-          type='text' 
-          placeholder='Название поста' 
-        />
-        <MyInput 
-          value={post.body}
-          onChange={e => setPost({...post,  body: e.target.value})}
-          type='text' 
-          placeholder='Описание поста' 
-        />
-        {/* Неуправляемый\Неконтролируемый компонент */}
-        {/* <MyInput 
-          ref={bodyInputRef}
-          type='text' 
-          placeholder='Описание поста' 
-        /> */}
-        <MyButton onClick={addNewPost}>Создать пост</MyButton>
-      </form>
-      <PostList posts={posts} title='Посты про JS' />
+      <PostForm create={createPost} />
+      <PostList remove={removePost} posts={posts} title='Посты про JS' />
     </div>
   );
 }
