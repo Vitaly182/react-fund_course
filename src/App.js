469e93676e8e4df733b47727a2b262c44ad46f6e
Vitaly <w.182@mail.ru>
2022-01-20 01:06:19 +0300

    Поиск. Фильтрация

--- src/App.js
+++ src/App.js
@@ -17,6 +17,17 @@ function App() {
   ])
 
   const [selectedSort, setSelectedSort] = useState('')
+  const [searchQuery, setSearchQuery] = useState('')
+
+  function getSortedPosts() {
+    console.log('Отработала функция getSortedPosts()')
+    if (selectedSort) {
+      return [...posts].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]))
+    }
+    return posts;
+  }
+
+  const sortedPosts = getSortedPosts()
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
@@ -29,7 +40,6 @@ function App() {
 
   const sortPosts = (sort) => {
     setSelectedSort(sort);
-    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
   }
 
   return (
@@ -37,6 +47,11 @@ function App() {
       <PostForm create={createPost} />
       <hr style={{margin: '15px 0'}} />
       <div>
+        <MyInput 
+          value={searchQuery}
+          onChange={e => setSearchQuery(e.target.value)}
+          placeholder="Поиск..."
+        />
         <MySelect 
           value={selectedSort}
           onChange={sortPosts}
@@ -49,7 +64,7 @@ function App() {
       </div>
       {posts.length 
         ? 
-        <PostList remove={removePost} posts={posts} title='Посты про JS' />
+        <PostList remove={removePost} posts={sortedPosts} title='Посты про JS' />
         : 
         <h1 style={{textAlign: "center"}}>
           Посты не найдены! 
