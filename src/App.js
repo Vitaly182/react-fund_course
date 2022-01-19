e9ee4e132b2f80c63f7449ee53e860f8255ec2f4
Vitaly <w.182@mail.ru>
2022-01-19 23:58:55 +0300

    Отрисовка по условию

--- src/App.js
+++ src/App.js
@@ -29,7 +29,14 @@ function App() {
   return (
     <div className="App">
       <PostForm create={createPost} />
-      <PostList remove={removePost} posts={posts} title='Посты про JS' />
+      {posts.length 
+        ? 
+        <PostList remove={removePost} posts={posts} title='Посты про JS' />
+        : 
+        <h1 style={{textAlign: "center"}}>
+          Посты не найдены! 
+        </h1>
+      }
     </div>
   );
 }
