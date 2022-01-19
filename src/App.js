9f5220bc49c75eb4dc47508b5742cc8eb2fe95a4
Vitaly <w.182@mail.ru>
2022-01-19 03:28:24 +0300

    Работы сос списками. Преобразование массива объектов в массив React элементов

--- src/App.js
+++ src/App.js
@@ -2,18 +2,26 @@ import React, { useState } from "react";
 import ClassCounter from "./components/ClassCounter";
 import Counter from "./components/Counter";
 import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
 import './styles/App.css';
 
 function App() {
-  
+  const [posts_1, setPOsts_1] = useState([
+    {id: 1, title: 'Javascript 1', body: 'Description 1'},
+    {id: 2, title: 'Javascript 2', body: 'Description 2'},
+    {id: 3, title: 'Javascript 3', body: 'Description 3'},
+  ])
+  const [posts_2, setPOsts_2] = useState([
+    {id: 1, title: 'Python 1', body: 'Description 1'},
+    {id: 2, title: 'Python 2', body: 'Description 2'},
+    {id: 3, title: 'Python 3', body: 'Description 3'},
+  ])
 
 
   return (
     <div className="App">
-      <PostItem post={{id: 1, title: 'Javascript', body: 'Description'}}/>
-      <PostItem post={{id: 2, title: 'Javascript', body: 'Description'}}/>
-      <PostItem post={{id: 3, title: 'Javascript', body: 'Description'}}/>
-
+      <PostList posts={posts_1} title='Посты про JS' />
+      <PostList posts={posts_2} title='Посты про Python' />
     </div>
   );
 }
