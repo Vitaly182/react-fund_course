9f6d5d807f1bc44cdc8e01dc708bc2ba02d7a648
Vitaly <w.182@mail.ru>
2022-01-23 01:31:04 +0300

    Загрузка комментариев к посту

--- src/pages/PostIdPage.jsx
+++ src/pages/PostIdPage.jsx
@@ -7,13 +7,19 @@ import { useFetching } from '../hooks/useFetching';
 const PostIdPage = () => {
     const params = useParams()
     const [post, setPost] = useState({});
+    const [comments, setComments] = useState([]);
     const [fetchPostById, isLoading, error] = useFetching( async (id) => {
         const response = await PostService.getById(id)
         setPost(response.data);
     })
+    const [fetchComments, isComLoading, comError] = useFetching( async (id) => {
+        const response = await PostService.getCommentsByPostId(id)
+        setComments(response.data);
+    })
 
     useEffect(() => {
         fetchPostById(params.id)
+        fetchComments(params.id)
     }, [])
 
 
@@ -25,6 +31,20 @@ const PostIdPage = () => {
             {isLoading
                 ? <Loader/>
                 : <div>{post.id}. {post.title}</div>
+            }
+            <h1>
+                Комментарии
+            </h1>
+            {isComLoading
+                ? <Loader/>
+                : <div>
+                    {comments.map(comm =>
+                        <div style={{marginTop: 15}}>
+                            <h5>{comm.email}</h5>
+                            <div>{comm.body}</div>
+                        </div>
+                    )}
+                </div>
             }
         </div>
     );
