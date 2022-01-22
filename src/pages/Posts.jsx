57dcac6f915f7165bb0c28a72f67ad69f12926f1
Vitaly <w.182@mail.ru>
2022-01-22 04:16:50 +0300

    React router. Постраничная навигация. BrowserRouter, Routes, Route, Navigate

---
+++ src/pages/Posts.jsx
@@ -0,0 +1,80 @@
+import React, { useEffect, useMemo, useRef, useState } from "react";
+import PostList from "../components/PostList";
+import MyButton from "../components/UI/button/MyButton";
+import PostForm from "../components/PostForm";
+import PostFilter from "../components/PostFilter";
+import MyModal from "../components/UI/MyModal/MyModal";
+import { usePosts } from "../hooks/usePosts";
+import PostService from "../API/PostService";
+import Loader from "../components/UI/Loader/Loader";
+import { useFetching } from "../hooks/useFetching";
+import { getPageCount } from "../utils/pages";
+import Pagination from "../components/UI/pagination/Pagination";
+
+function Posts() {
+    const [posts, setPosts] = useState([])
+    const [filter, setFilter] = useState({sort: '', query: ''})
+    const [modal, setModal] = useState(false);
+    const [totalPages, setTotalPages] = useState(0);
+    const [limit, setLimit] = useState(10);
+    const [page, setPage] = useState(1);
+    const sortedAndSearchPosts = usePosts(posts, filter.sort, filter.query);
+
+    const [fetchPosts, isPostsLoading, postError] = useFetching( async (limit, page) => {
+    const response = await PostService.getAll(limit, page);
+    setPosts(response.data)
+    const totalCount = response.headers['x-total-count']
+    setTotalPages(getPageCount(totalCount, limit))
+})
+
+console.log(totalPages)
+
+useEffect(() => {
+    fetchPosts(limit, page)
+}, [])
+
+const createPost = (newPost) => {
+    setPosts([...posts, newPost])
+    setModal(false)
+}
+
+  //Получаем post из дочернего компонента
+const removePost = (post) => {
+    setPosts(posts.filter(p => p.id !== post.id))
+}
+
+const changePage = (page) => {
+    setPage(page)
+    fetchPosts(limit, page)
+}
+
+return (
+    <div className="App">
+    <button onClick={fetchPosts}>GET POSTS</button>
+    <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
+        Создать пост
+    </MyButton>
+    <MyModal visible={modal} setVisible={setModal}>
+        <PostForm create={createPost} />
+    </MyModal>      
+    <hr style={{margin: '15px 0'}} />
+    <PostFilter 
+        filter={filter} 
+        setFilter={setFilter} 
+    />
+    {postError &&
+        <h1>Произошла ошибка ${postError}</h1>}
+    {isPostsLoading
+        ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}><Loader/></div>
+        : <PostList remove={removePost} posts={sortedAndSearchPosts} title='Посты про JS' />
+    } 
+    <Pagination 
+        page={page} 
+        changePage={changePage} 
+        totalPages={totalPages}
+    />
+    </div>
+    );
+}
+
+export default Posts;
