efde3fcebd6451d829865f62a51db02503b7f97e
Vitaly <w.182@mail.ru>
2022-01-23 02:56:57 +0300

    Улучшаем навигацию. Приватные и публичные маршруты

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -4,16 +4,36 @@ import About from "../pages/About";
 import Posts from "../pages/Posts";
 import Error from "../pages/Error";
 import PostIdPage from '../pages/PostIdPage';
+import { publicRoutes, privateRoutes } from '../router';
 
 const AppRouter = () => {
+    const isAuth = false;
     return (
-        <Routes>
-            <Route path="/about"  element={<About />} />
-            <Route exact path="/posts"  element={<Posts />} />
-            <Route exact path="/posts/:id"  element={<PostIdPage />} />
-            <Route path="/error"  element={<Error />} />
-            <Route path='*' element={<Navigate replace to="/error" />} />
-        </Routes>
+        isAuth 
+            ?
+            <Routes>
+                {privateRoutes.map(route =>
+                    <Route 
+                        key={route.element}
+                        element={<route.element/>}
+                        path={route.path}
+                        exact={route.exact}
+                    />
+                )}
+                <Route path='*' element={<Navigate replace to="/posts" />} />
+            </Routes>
+            :
+            <Routes>
+                {publicRoutes.map(route =>
+                    <Route 
+                        key={route.element}
+                        element={<route.element/>}
+                        path={route.path}
+                        exact={route.exact}
+                    />
+                )}
+                <Route path='*' element={<Navigate replace to="/login" />} />
+            </Routes>
     );
 };
 
