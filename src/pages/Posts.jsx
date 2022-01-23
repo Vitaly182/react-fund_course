9c51ef41974fa31426ca55604cd9ccd938b29cad
Vitaly <w.182@mail.ru>
2022-01-23 04:17:43 +0300

    Бесконечная лента. Динамическая пагинация. useObserver

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -10,6 +10,8 @@ import Loader from "../components/UI/Loader/Loader";
 import { useFetching } from "../hooks/useFetching";
 import { getPageCount } from "../utils/pages";
 import Pagination from "../components/UI/pagination/Pagination";
+import { useObserver } from "../hooks/useObserver";
+import MySelect from "../components/UI/select/MySelect";
 
 function Posts() {
     const [posts, setPosts] = useState([])
@@ -19,19 +21,25 @@ function Posts() {
     const [limit, setLimit] = useState(10);
     const [page, setPage] = useState(1);
     const sortedAndSearchPosts = usePosts(posts, filter.sort, filter.query);
+    const lastElement = useRef()
+    // console.log(lastElement)
 
     const [fetchPosts, isPostsLoading, postError] = useFetching( async (limit, page) => {
     const response = await PostService.getAll(limit, page);
-    setPosts(response.data)
+    setPosts([...posts, ...response.data])
     const totalCount = response.headers['x-total-count']
     setTotalPages(getPageCount(totalCount, limit))
 })
 
 // console.log(totalPages)
 
+useObserver(lastElement, page < totalPages, isPostsLoading, () => {
+    setPage(page + 1);
+})
+
 useEffect(() => {
     fetchPosts(limit, page)
-}, [])
+}, [page, limit])
 
 const createPost = (newPost) => {
     setPosts([...posts, newPost])
@@ -45,7 +53,6 @@ const removePost = (post) => {
 
 const changePage = (page) => {
     setPage(page)
-    fetchPosts(limit, page)
 }
 
 return (
@@ -62,12 +69,24 @@ return (
         filter={filter} 
         setFilter={setFilter} 
     />
+    <MySelect
+        value={limit}
+        onChange={value => setLimit(value)}
+        defaultValue='Количество элементов на странице'
+        options={[
+            {value: 5, name: '5'},
+            {value: 10, name: '10'},
+            {value: 25, name: '25'},
+            {value: -1, name: 'Показать все'},
+        ]}
+    />
     {postError &&
         <h1>Произошла ошибка ${postError}</h1>}
-    {isPostsLoading
-        ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}><Loader/></div>
-        : <PostList remove={removePost} posts={sortedAndSearchPosts} title='Посты про JS' />
-    } 
+    <PostList remove={removePost} posts={sortedAndSearchPosts} title='Посты про JS' />  
+    <div ref={lastElement} style={{height: 20, background: 'red'}} />
+    {isPostsLoading &&
+        <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}><Loader/></div>
+    }
     <Pagination 
         page={page} 
         changePage={changePage} 
