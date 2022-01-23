6574d09ca7ae43179ea2606ece44a435e0357239
Vitaly <w.182@mail.ru>
2022-01-23 03:36:29 +0300

    useContext Глобальные данные. Авторизация пользователя

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter } from "react-router-dom";
 import './styles/App.css';
 import Navbar from "./components/UI/Navbar/Navbar";
 import AppRouter from "./components/AppRouter";
+import { AuthContext } from "./context";
+import { useState } from "react/cjs/react.development";
 
 function App() {
+  const [isAuth, setIsAuth] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if(localStorage.getItem('auth')) {
+      setIsAuth(true)
+    }
+    setIsLoading(false);
+  }, [])
+
   return(
-    <BrowserRouter>
-      <Navbar/>
-      <AppRouter/>
-    </BrowserRouter>
+    <AuthContext.Provider value={{
+      isAuth,
+      setIsAuth,
+      isLoading
+    }}>
+      <BrowserRouter>
+        <Navbar/>
+        <AppRouter/>
+      </BrowserRouter>
+    </AuthContext.Provider>
   )
 
 }
--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { BrowserRouter, Link, Routes, Route, Navigate } from "react-router-dom";
 import About from "../pages/About";
 import Posts from "../pages/Posts";
 import Error from "../pages/Error";
 import PostIdPage from '../pages/PostIdPage';
 import { publicRoutes, privateRoutes } from '../router';
+import { AuthContext } from '../context';
+import Loader from './UI/Loader/Loader';
 
 const AppRouter = () => {
-    const isAuth = false;
+    const {isAuth, isLoading} = useContext(AuthContext);
+    // console.log(isAuth)
+
+    if(isLoading) {
+        return <Loader/>
+    }
+
     return (
         isAuth 
             ?
             <Routes>
                 {privateRoutes.map(route =>
                     <Route 
-                        key={route.element}
+                        key={route.path}
                         element={<route.element/>}
                         path={route.path}
                         exact={route.exact}
